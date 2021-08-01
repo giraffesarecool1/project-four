0df10b69fc2c415e3026cbbabb44d3740b99e5f7
nickynite <nbeullac@gmail.com>
2021-08-01 12:36:24 -0500

    Added styles to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,20 @@
 const Header = () => {
   return (
     <header className="header">
-      <div className="headerContainer">
-        {/* <img src="" alt="" /> */}
-        <h1 className="headerTitle">Podcast Planner</h1>
-        <p className="headerText">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam numquam voluptatibus provident cum rem illo?</p>
-        <a className="button" href="#getStarted">Get Started</a>
-      </div>
+      
+      <div className="headerContainer wrapper">
+
+        <img src="headphonesCC.jpg" alt="a pair of black headphones" />
+      
+        <div className="textInHeader">
+          <h1 className="headerTitle">Podcast<img src="soundwhite.svg" className="logoWhite" alt="Podcast Planner logo" />Planner</h1>
+          <p className="headerText"><p>Search for podcasts based on your commute time.</p> Listen while you walk, hike or commute to work and save them to a playlist!</p>
+        </div>
+
+      </div> 
+
+      <a className="button" href="#getStarted">Get Started</a>
+      
     </header>
   )
 }
