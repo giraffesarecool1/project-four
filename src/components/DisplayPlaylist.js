5b9e04582a98cdd61fa545112c76498db2b61d37
Beth <beth.whalen@gmail.com>
2021-08-05 20:18:34 -0300

    initial commit of displayPlaylist

---
+++ src/components/DisplayPlaylist.js
@@ -0,0 +1,20 @@
+// PSEUDO CODE:
+// playlists have been saved to firebase
+// want to press a button that will display the playlist(s)
+// user must choose a playlist
+    // will need some sort of menu that displays the users playlists
+    // Discuss with group - a menu that slides out / a dropdown / ?
+// once clicked, the playlist (podcasts) displays on screen
+    // ensure that if any search results are currently being displayed, they are "replaced" by the users chosen playlist
+
+import firebase from "../firebase";
+import {useState} from 'react';
+
+const DisplayPlaylist = () => {
+
+
+
+
+}
+
+export default DisplayPlaylist;
