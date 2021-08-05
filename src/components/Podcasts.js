5b92fe86ece8e86e6f711774d6d4378a9b0cd212
Chris Leung <cleungdev@gmail.com>
2021-08-05 17:46:00 -0400

    added link to podcast and updated visited links to black

--- src/components/Podcasts.js
+++ src/components/Podcasts.js
@@ -18,12 +18,14 @@ const Podcasts = (props) => {
           podcasts.map((podcast) => {
             return (
               <li className="podcastsCatalogueItem" key={podcast.id}>
-                <img src={podcast.thumbnail} alt={podcast.title} />
-                <p className="podcastsCatalogueItemTitle">{podcast.title_original}</p>
-                <div className="podcastsCatalogueItemDesc">
-                  <div className="podcastsCatalogueItemDescTruncate">{podcast.description_original}</div>
-                </div>
-                <div className="podcastsCatalogueItemLength">Length: {Math.floor(podcast.audio_length_sec / 60)} min</div>
+                <a href={podcast.listennotes_url} target="_blank" rel="noopener noreferrer">
+                  <img src={podcast.thumbnail} alt={podcast.title} />
+                  <p className="podcastsCatalogueItemTitle">{podcast.title_original}</p>
+                  <div className="podcastsCatalogueItemDesc">
+                    <div className="podcastsCatalogueItemDescTruncate">{podcast.description_original}</div>
+                  </div>
+                  <div className="podcastsCatalogueItemLength">Length: {Math.floor(podcast.audio_length_sec / 60)} min</div>
+                </a>
                 
                 <AddFavorite />
               </li>
