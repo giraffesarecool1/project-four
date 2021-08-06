1c3c9f7964a74e400ec6f1ecf4e9b128b20aee52
nickynite <nbeullac@gmail.com>
2021-08-05 21:46:33 -0500

    added media queries to header

--- src/components/PlaylistLink.js
+++ src/components/PlaylistLink.js
@@ -3,7 +3,7 @@ import soundpurple from '../assets/soundpurple.png';
 const PlaylistLink = () => {
     return(
 
-        <div className="playlistLink">
+        <div className="playlistLink savePlaylistWrapper">
 
             <img src={soundpurple} alt="purple logo" className="purpleLogo" />
             <a href="#playlists">View your playlists</a>
--- src/components/SavePlaylist.js
+++ src/components/SavePlaylist.js
@@ -48,7 +48,6 @@ const SavePlaylist = (props) => {
             </form>
         </section>
     )
-
 }
 
 export default SavePlaylist;
