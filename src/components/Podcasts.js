2abbd4e0ace6802d6826708a8a974ec0c925e2fd
giraffesarecool1 <73612023+giraffesarecool1@users.noreply.github.com>
2022-08-04 20:54:52 -0400

    Update Podcasts.js

--- src/components/Podcasts.js
+++ src/components/Podcasts.js
@@ -28,7 +28,7 @@ const Podcasts = (props) => {
                       <img src={podcast.thumbnail} alt={podcast.title} />
                       <p className="podcastsCatalogueItemTitle">{podcast.title_original}</p>
                       <div className="podcastsCatalogueItemDesc">
-                        <div className="podcastsCatalogueItemDescTruncate">{podcast.description_original}</div>
+                        <div className="podcastsCatalogueItemDescTruncate">{podcast.description_original.replaceAll(<[^>]*>g, '')}</div>
                       </div>
                       <div className="podcastsCatalogueItemLength">Length: {Math.floor(podcast.audio_length_sec / 60)} min</div>
                     </a>
